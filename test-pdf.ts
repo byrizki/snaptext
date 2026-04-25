15411808cd8fd35894399b1b4baf5cf57533778d
Muhamad Rizki <muhrizkie88@gmail.com>
2026-04-25 09:42:47 +0700

    cleanup

--- test-pdf.ts
+++
@@ -1,8 +0,0 @@
-import { definePDFJSModule, getResolvedPDFJS } from "unpdf";
-
-async function main() {
-  await definePDFJSModule(() => import('pdfjs-dist/legacy/build/pdf.js'));
-  const { version } = await getResolvedPDFJS();
-  console.log("PDF.js legacy version: ", version);
-}
-main().catch(console.error);
