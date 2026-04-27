64012ab31e3cb5e43e5dd1fc21762eabf12842c3
Muhamad Rizki <muhrizkie88@gmail.com>
2026-04-27 08:57:04 +0700

    feat(auth): initialize authentication with better-auth and integrate middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,14 +4,43 @@ import { getDb } from "@/db";
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(getDb(), {
-        provider: "pg",
-        schema: {
-            ...schema
-        }
-    }),
-    emailAndPassword: {
-        enabled: true,
-        disableSignUp: true,
+  database: drizzleAdapter(getDb(), {
+    provider: "pg",
+    schema: {
+      ...schema,
     },
+  }),
+  emailAndPassword: {
+    enabled: true,
+    disableSignUp: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "user",
+        input: false,
+      },
+    },
+  },
+  databaseHooks: {
+    user: {
+      create: {
+        before: async (user) => {
+          return {
+            data: {
+              ...user,
+              role: "user",
+            },
+          };
+        },
+      },
+    },
+  },
 });
